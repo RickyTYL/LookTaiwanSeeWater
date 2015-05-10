38188397e49248046a12e508ae79e5bb8e1af0c6
YiChiaTsai <aaron.yctsai@gmail.com>
2015-05-11 00:00:06 +0800

    Testing reservoir

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ $(function () {
         url: "http://128.199.223.114:10080/today",
         dataType: "jsonp",
         success: function (data) {
-            
+
             //data.data[0] 石門水庫(新北、桃園、新竹)
             //data.data[1] 新山水庫(基隆)
             //data.data[2] 翡翠水庫(台北、新北)
@@ -56,73 +56,85 @@ $(function () {
             //data.data[18] 阿公店水庫(高雄、洩洪至二仁溪)
             //data.data[19] 牡丹水庫(屏東)
             //data.data[20] 成功水庫(澎湖)
-            
+
+            var config0 = liquidFillGaugeDefaultSettings();
+            loadLiquidFillGauge("fillgauge0", Number(data.data[0].immediatePercentage.replace('%','')), config0);
             var config1 = liquidFillGaugeDefaultSettings();
-            config1.circleColor = "#FF7777";
-            config1.textColor = "#FF4444";
-            config1.waveTextColor = "#FFAAAA";
-            config1.waveColor = "#FFDDDD";
-            config1.circleThickness = 0.2;
-            config1.textVertPosition = 0.2;
-            config1.waveAnimateTime = 1000;
-            loadLiquidFillGauge("fillgauge1", Number(data.data[0].immediatePercentage.replace('%','')), config1);
-            
+            loadLiquidFillGauge("fillgauge1", Number(data.data[1].immediatePercentage.replace('%','')), config1);
             var config2 = liquidFillGaugeDefaultSettings();
-            config2.circleColor = "#D4AB6A";
-            config2.textColor = "#553300";
-            config2.waveTextColor = "#805615";
-            config2.waveColor = "#AA7D39";
-            config2.circleThickness = 0.1;
-            config2.circleFillGap = 0.2;
-            config2.textVertPosition = 0.8;
-            config2.waveAnimateTime = 2000;
-            config2.waveHeight = 0.3;
-            config2.waveCount = 1;
-            loadLiquidFillGauge("fillgauge2", Number(data.data[2].immediatePercentage.replace('%','')), config2);
-            
+            loadLiquidFillGauge("fillgauge2", Number(data.data[].immediatePercentage.replace('%','')), config2);
             var config3 = liquidFillGaugeDefaultSettings();
-            config3.textVertPosition = 0.8;
-            config3.waveAnimateTime = 5000;
-            config3.waveHeight = 0.15;
-            config3.waveAnimate = false;
-            config3.waveOffset = 0.25;
-            config3.valueCountUp = false;
-            config3.displayPercent = false;
-            loadLiquidFillGauge("fillgauge3", Number(data.data[3].immediatePercentage.replace('%','')), config3);
-            
+            loadLiquidFillGauge("fillgauge3", Number(data.data[].immediatePercentage.replace('%','')), config3);
             var config4 = liquidFillGaugeDefaultSettings();
-            config4.circleThickness = 0.15;
-            config4.circleColor = "#808015";
-            config4.textColor = "#555500";
-            config4.waveTextColor = "#FFFFAA";
-            config4.waveColor = "#AAAA39";
-            config4.textVertPosition = 0.8;
-            config4.waveAnimateTime = 1000;
-            config4.waveHeight = 0.05;
-            config4.waveAnimate = true;
-            config4.waveRise = false;
-            config4.waveOffset = 0.25;
-            config4.textSize = 0.75;
-            config4.waveCount = 3;
-            loadLiquidFillGauge("fillgauge4", Number(data.data[14].immediatePercentage.replace('%','')), config4);
-            
-            var config5 = liquidFillGaugeDefaultSettings();
-            config5.circleThickness = 0.4;
-            config5.circleColor = "#6DA398";
-            config5.textColor = "#0E5144";
-            config5.waveTextColor = "#6DA398";
-            config5.waveColor = "#246D5F";
-            config5.textVertPosition = 0.52;
-            config5.waveAnimateTime = 5000;
-            config5.waveHeight = 0;
-            config5.waveAnimate = false;
-            config5.waveCount = 2;
-            config5.waveOffset = 0.25;
-            config5.textSize = 1.2;
-            config5.minValue = 30;
-            config5.maxValue = 150
-            config5.displayPercent = false;
-            loadLiquidFillGauge("fillgauge5", Number(data.data[15].immediatePercentage.replace('%','')), config5);
+            loadLiquidFillGauge("fillgauge4", Number(data.data[].immediatePercentage.replace('%','')), config4);
+
+
+            var config10 = liquidFillGaugeDefaultSettings();
+            config10.circleColor = "#FF7777";
+            config10.textColor = "#FF4444";
+            config10.waveTextColor = "#FFAAAA";
+            config10.waveColor = "#FFDDDD";
+            config10.circleThickness = 0.2;
+            config10.textVertPosition = 0.2;
+            config10.waveAnimateTime = 1000;
+            loadLiquidFillGauge("fillgauge1", Number(data.data[0].immediatePercentage.replace('%','')), config10);
+
+            var config20 = liquidFillGaugeDefaultSettings();
+            config20.circleColor = "#D4AB6A";
+            config20.textColor = "#553300";
+            config20.waveTextColor = "#805615";
+            config20.waveColor = "#AA7D39";
+            config20.circleThickness = 0.1;
+            config20.circleFillGap = 0.2;
+            config20.textVertPosition = 0.8;
+            config20.waveAnimateTime = 2000;
+            config20.waveHeight = 0.3;
+            config20.waveCount = 1;
+            loadLiquidFillGauge("fillgauge2", Number(data.data[2].immediatePercentage.replace('%','')), config20);
+
+            var config30 = liquidFillGaugeDefaultSettings();
+            config30.textVertPosition = 0.8;
+            config30.waveAnimateTime = 5000;
+            config30.waveHeight = 0.15;
+            config30.waveAnimate = false;
+            config30.waveOffset = 0.25;
+            config30.valueCountUp = false;
+            config30.displayPercent = false;
+            loadLiquidFillGauge("fillgauge3", Number(data.data[3].immediatePercentage.replace('%','')), config30);
+
+            var config40 = liquidFillGaugeDefaultSettings();
+            config40.circleThickness = 0.15;
+            config40.circleColor = "#808015";
+            config40.textColor = "#555500";
+            config40.waveTextColor = "#FFFFAA";
+            config40.waveColor = "#AAAA39";
+            config40.textVertPosition = 0.8;
+            config40.waveAnimateTime = 1000;
+            config40.waveHeight = 0.05;
+            config40.waveAnimate = true;
+            config40.waveRise = false;
+            config40.waveOffset = 0.25;
+            config40.textSize = 0.75;
+            config40.waveCount = 3;
+            loadLiquidFillGauge("fillgauge4", Number(data.data[14].immediatePercentage.replace('%','')), config40);
+
+            var config50 = liquidFillGaugeDefaultSettings();
+            config50.circleThickness = 0.4;
+            config50.circleColor = "#6DA398";
+            config50.textColor = "#0E5144";
+            config50.waveTextColor = "#6DA398";
+            config50.waveColor = "#246D5F";
+            config50.textVertPosition = 0.52;
+            config50.waveAnimateTime = 5000;
+            config50.waveHeight = 0;
+            config50.waveAnimate = false;
+            config50.waveCount = 2;
+            config50.waveOffset = 0.25;
+            config50.textSize = 1.2;
+            config50.minValue = 30;
+            config50.maxValue = 150
+            config50.displayPercent = false;
+            loadLiquidFillGauge("fillgauge5", Number(data.data[15].immediatePercentage.replace('%','')), config50);
         }
     });
 });
