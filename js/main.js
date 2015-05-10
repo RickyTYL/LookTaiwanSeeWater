5e04f1f0f867c7f338249d8f8b893114d990627b
YiChiaTsai <aaron.yctsai@gmail.com>
2015-05-11 03:09:05 +0800

    Update reservoirs11 by aaron

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ $(function () {
     });
 });
 
-<!-- Script to Activate the Carousel -->
+// Script to Activate the Carousel
 <script>
 $('.carousel').carousel({
     interval: 5000 //changes the speed
