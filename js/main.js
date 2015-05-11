c0ca2e936e9eda98a69e855d86cd9d843927984f
Ray0427 <ray13242000@gmail.com>
2015-05-11 15:29:37 +0800

    main.js for_loop&color

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // Closes the sidebar menu
 $("#menu-close").click(function (e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
+	e.preventDefault();
+	$("#sidebar-wrapper").toggleClass("active");
 });
 
 // Opens the sidebar menu
 $("#menu-toggle").click(function (e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
+	e.preventDefault();
+	$("#sidebar-wrapper").toggleClass("active");
 });
 
 // Scrolls to the selected menu item on the page
@@ -31,11 +31,11 @@ $(function () {
 
 //ajax get water reservoir data
 $(function () {
-    $.ajax({
-        type: "GET",
-        url: "http://128.199.223.114:10080/today",
-        dataType: "jsonp",
-        success: function (data) {
+	$.ajax({
+		type: "GET",
+		url: "http://128.199.223.114:10080/today",
+		dataType: "jsonp",
+		success: function (data) {
 
             //data.data[0] 石門水庫(新北、桃園、新竹)
             //data.data[1] 新山水庫(基隆)
@@ -59,121 +59,144 @@ $(function () {
             //data.data[19] 牡丹水庫(屏東)
             //data.data[20] 成功水庫(澎湖)
 
-            var config0 = liquidFillGaugeDefaultSettings();
-        
-              config0.circleColor = "#FF7777";
-              config0.textColor = "#FF4444";
-              config0.waveTextColor = "#FFAAAA";
-              config0.waveColor = "#FFDDDD";
+            var config = liquidFillGaugeDefaultSettings();
+            for (var i = 0; i < data.data.length; i++) {
+            	if(data.data[i].immediatePercentage.replace('%','') <= 30){
+            		config.circleColor = "#FF7777";
+            		config.textColor = "#FF4444";
+            		config.waveTextColor = "#FFAAAA";
+            		config.waveColor = "#FFDDDD";
+            	}
+            	else if(data.data[i].immediatePercentage.replace('%','') <= 60){
+            		config.circleColor = "#808015";
+            		config.textColor = "#555500";
+            		config.waveTextColor = "#FFFFAA";
+            		config.waveColor = "#AAAA39";
+            	}
+            	else {
+            		config = liquidFillGaugeDefaultSettings();
+            	}
 
-            loadLiquidFillGauge("fillgauge0", Number(data.data[0].immediatePercentage.replace('%','')), config0);
+            	loadLiquidFillGauge('fillgauge'+i, Number(data.data[i].immediatePercentage.replace('%','')), config);
+            };
+            // var config1 = liquidFillGaugeDefaultSettings();
+            // if(data.data[1].immediatePercentage.replace('%','') <= 30){
+            // 	config1.circleColor = "#FF7777";
+            // 	config1.textColor = "#FF4444";
+            // 	config1.waveTextColor = "#FFAAAA";
+            // 	config1.waveColor = "#FFDDDD";
+            // }
+            // else if(data.data[1].immediatePercentage.replace('%','') <= 60){
+            // 	config1.circleColor = "#808015";
+            // 	config1.textColor = "#555500";
+            // 	config1.waveTextColor = "#FFFFAA";
+            // 	config1.waveColor = "#AAAA39";
+            // }
+            // loadLiquidFillGauge("fillgauge1", Number(data.data[1].immediatePercentage.replace('%','')), config1);
 
-            var config1 = liquidFillGaugeDefaultSettings();
-            if(data.data[1].immediatePercentage <= 30){
-              config1.circleColor = "#FF7777";
-              config1.textColor = "#FF4444";
-              config1.waveTextColor = "#FFAAAA";
-              config1.waveColor = "#FFDDDD";
-            }
-            else if(data.data[1].immediatePercentage <= 60){
-              config1.circleColor = "#808015";
-              config1.textColor = "#555500";
-              config1.waveTextColor = "#FFFFAA";
-              config1.waveColor = "#AAAA39";
-            }
-            loadLiquidFillGauge("fillgauge1", Number(data.data[1].immediatePercentage.replace('%','')), config1);
+            // var config2 = liquidFillGaugeDefaultSettings();
+            // if(data.data[2].immediatePercentage <= 30){
+            // 	config2.circleColor = "#FF7777";
+            // 	config2.textColor = "#FF4444";
+            // 	config2.waveTextColor = "#FFAAAA";
+            // 	config2.waveColor = "#FFDDDD";
+            // }
+            // else if(data.data[2].immediatePercentage <= 60){
+            // 	config2.circleColor = "#808015";
+            // 	config2.textColor = "#555500";
+            // 	config2.waveTextColor = "#FFFFAA";
+            // 	config2.waveColor = "#AAAA39";
+            // }
+            // loadLiquidFillGauge("fillgauge2", Number(data.data[2].immediatePercentage.replace('%','')), config2);
 
-            var config2 = liquidFillGaugeDefaultSettings();
-            if(data.data[2].immediatePercentage <= 30){
-              config2.circleColor = "#FF7777";
-              config2.textColor = "#FF4444";
-              config2.waveTextColor = "#FFAAAA";
-              config2.waveColor = "#FFDDDD";
-            }
-            else if(data.data[2].immediatePercentage <= 60){
-              config2.circleColor = "#808015";
-              config2.textColor = "#555500";
-              config2.waveTextColor = "#FFFFAA";
-              config2.waveColor = "#AAAA39";
-            }
-            loadLiquidFillGauge("fillgauge2", Number(data.data[2].immediatePercentage.replace('%','')), config2);
+            // var config3 = liquidFillGaugeDefaultSettings();
+            // if(data.data[3].immediatePercentage <= 30){
+            // 	config3.circleColor = "#FF7777";
+            // 	config3.textColor = "#FF4444";
+            // 	config3.waveTextColor = "#FFAAAA";
+            // 	config3.waveColor = "#FFDDDD";
+            // }
+            // else if(data.data[3].immediatePercentage <= 60){
+            // 	config3.circleColor = "#808015";
+            // 	config3.textColor = "#555500";
+            // 	config3.waveTextColor = "#FFFFAA";
+            // 	config3.waveColor = "#AAAA39";
+            // }
+            // loadLiquidFillGauge("fillgauge3", Number(data.data[3].immediatePercentage.replace('%','')), config3);
 
-            var config3 = liquidFillGaugeDefaultSettings();
-            if(data.data[3].immediatePercentage <= 30){
-              config3.circleColor = "#FF7777";
-              config3.textColor = "#FF4444";
-              config3.waveTextColor = "#FFAAAA";
-              config3.waveColor = "#FFDDDD";
-            }
-            else if(data.data[3].immediatePercentage <= 60){
-              config3.circleColor = "#808015";
-              config3.textColor = "#555500";
-              config3.waveTextColor = "#FFFFAA";
-              config3.waveColor = "#AAAA39";
-            }
-            loadLiquidFillGauge("fillgauge3", Number(data.data[3].immediatePercentage.replace('%','')), config3);
-
-            var config4 = liquidFillGaugeDefaultSettings();
+            // var config4 = liquidFillGaugeDefaultSettings();
             
-              config4.circleColor = "#808015";
-              config4.textColor = "#555500";
-              config4.waveTextColor = "#FFFFAA";
-              config4.waveColor = "#AAAA39";
+            // config4.circleColor = "#808015";
+            // config4.textColor = "#555500";
+            // config4.waveTextColor = "#FFFFAA";
+            // config4.waveColor = "#AAAA39";
             
-            loadLiquidFillGauge("fillgauge4", Number(data.data[4].immediatePercentage.replace('%','')), config4);
+            // loadLiquidFillGauge("fillgauge4", Number(data.data[4].immediatePercentage.replace('%','')), config4);
             
-            var config5 = liquidFillGaugeDefaultSettings();
-            config5.circleColor = "#FF7777";
-              config5.textColor = "#FF4444";
-              config5.waveTextColor = "#FFAAAA";
-              config5.waveColor = "#FFDDDD";
-            loadLiquidFillGauge("fillgauge5", Number(data.data[5].immediatePercentage.replace('%','')), config5);
-            var config6 = liquidFillGaugeDefaultSettings();
-            config6.circleColor = "#FF7777";
-              config6.textColor = "#FF4444";
-              config6.waveTextColor = "#FFAAAA";
-              config6.waveColor = "#FFDDDD";
-            loadLiquidFillGauge("fillgauge6", Number(data.data[6].immediatePercentage.replace('%','')), config6);
-            var config7 = liquidFillGaugeDefaultSettings();
-            config7.circleColor = "#FF7777";
-              config7.textColor = "#FF4444";
-              config7.waveTextColor = "#FFAAAA";
-              config7.waveColor = "#FFDDDD";
-            loadLiquidFillGauge("fillgauge7", Number(data.data[7].immediatePercentage.replace('%','')), config7);
-            var config8 = liquidFillGaugeDefaultSettings();
-              config8.circleColor = "#808015";
-              config8.textColor = "#555500";
-              config8.waveTextColor = "#FFFFAA";
-              config8.waveColor = "#AAAA39";
-            loadLiquidFillGauge("fillgauge8", Number(data.data[8].immediatePercentage.replace('%','')), config8);
-            var config9 = liquidFillGaugeDefaultSettings();
-              config9.circleColor = "#808015";
-              config9.textColor = "#555500";
-              config9.waveTextColor = "#FFFFAA";
-              config9.waveColor = "#AAAA39";
-            loadLiquidFillGauge("fillgauge9", Number(data.data[9].immediatePercentage.replace('%','')), config9);
-            var config60 = liquidFillGaugeDefaultSettings();
-            loadLiquidFillGauge("fillgauge10", Number(data.data[10].immediatePercentage.replace('%','')), config60);
-            var config11 = liquidFillGaugeDefaultSettings();
-            loadLiquidFillGauge("fillgauge11", Number(data.data[11].immediatePercentage.replace('%','')), config11);
-            var config12 = liquidFillGaugeDefaultSettings();
-            loadLiquidFillGauge("fillgauge12", Number(data.data[12].immediatePercentage.replace('%','')), config12);
-            var config13 = liquidFillGaugeDefaultSettings();
-            loadLiquidFillGauge("fillgauge13", Number(data.data[13].immediatePercentage.replace('%','')), config13);
-            var config14 = liquidFillGaugeDefaultSettings();
-            loadLiquidFillGauge("fillgauge14", Number(data.data[14].immediatePercentage.replace('%','')), config14);
-            var config15 = liquidFillGaugeDefaultSettings();
-            loadLiquidFillGauge("fillgauge15", Number(data.data[15].immediatePercentage.replace('%','')), config15);
-            var config16 = liquidFillGaugeDefaultSettings();
-            loadLiquidFillGauge("fillgauge16", Number(data.data[16].immediatePercentage.replace('%','')), config16);
-            var config17 = liquidFillGaugeDefaultSettings();
-            loadLiquidFillGauge("fillgauge17", Number(data.data[17].immediatePercentage.replace('%','')), config17);
-            var config18 = liquidFillGaugeDefaultSettings();
-            loadLiquidFillGauge("fillgauge18", Number(data.data[18].immediatePercentage.replace('%','')), config18);
-            var config19 = liquidFillGaugeDefaultSettings();
-            loadLiquidFillGauge("fillgauge19", Number(data.data[19].immediatePercentage.replace('%','')), config19);
-            var config70 = liquidFillGaugeDefaultSettings();
-            loadLiquidFillGauge("fillgauge20", Number(data.data[20].immediatePercentage.replace('%','')), config70);
+            // var config5 = liquidFillGaugeDefaultSettings();
+            // config5.circleColor = "#FF7777";
+            // config5.textColor = "#FF4444";
+            // config5.waveTextColor = "#FFAAAA";
+            // config5.waveColor = "#FFDDDD";
+            // loadLiquidFillGauge("fillgauge5", Number(data.data[5].immediatePercentage.replace('%','')), config5);
+            // var config6 = liquidFillGaugeDefaultSettings();
+            // config6.circleColor = "#FF7777";
+            // config6.textColor = "#FF4444";
+            // config6.waveTextColor = "#FFAAAA";
+            // config6.waveColor = "#FFDDDD";
+            // loadLiquidFillGauge("fillgauge6", Number(data.data[6].immediatePercentage.replace('%','')), config6);
+            // var config7 = liquidFillGaugeDefaultSettings();
+            // config7.circleColor = "#FF7777";
+            // config7.textColor = "#FF4444";
+            // config7.waveTextColor = "#FFAAAA";
+            // config7.waveColor = "#FFDDDD";
+            // loadLiquidFillGauge("fillgauge7", Number(data.data[7].immediatePercentage.replace('%','')), config7);
+            // var config8 = liquidFillGaugeDefaultSettings();
+            // config8.circleColor = "#808015";
+            // config8.textColor = "#555500";
+            // config8.waveTextColor = "#FFFFAA";
+            // config8.waveColor = "#AAAA39";
+            // loadLiquidFillGauge("fillgauge8", Number(data.data[8].immediatePercentage.replace('%','')), config8);
+            // var config9 = liquidFillGaugeDefaultSettings();
+            // config9.circleColor = "#808015";
+            // config9.textColor = "#555500";
+            // config9.waveTextColor = "#FFFFAA";
+            // config9.waveColor = "#AAAA39";
+            // loadLiquidFillGauge("fillgauge9", Number(data.data[9].immediatePercentage.replace('%','')), config9);
+            // var config10 = liquidFillGaugeDefaultSettings();
+            // if(data.data[10].immediatePercentage.replace('%','') <= 30){
+            // 	config10.circleColor = "#FF7777";
+            // 	config10.textColor = "#FF4444";
+            // 	config10.waveTextColor = "#FFAAAA";
+            // 	config10.waveColor = "#FFDDDD";
+            // }
+            // else if(data.data[10].immediatePercentage.replace('%','') <= 60){
+            // 	config10.circleColor = "#808015";
+            // 	config10.textColor = "#555500";
+            // 	config10.waveTextColor = "#FFFFAA";
+            // 	config10.waveColor = "#AAAA39";
+            // }
+            // loadLiquidFillGauge("fillgauge10", Number(data.data[10].immediatePercentage.replace('%','')), config10);
+            // var config11 = liquidFillGaugeDefaultSettings();
+            // loadLiquidFillGauge("fillgauge11", Number(data.data[11].immediatePercentage.replace('%','')), config11);
+            // var config12 = liquidFillGaugeDefaultSettings();
+            // loadLiquidFillGauge("fillgauge12", Number(data.data[12].immediatePercentage.replace('%','')), config12);
+            // var config13 = liquidFillGaugeDefaultSettings();
+            // loadLiquidFillGauge("fillgauge13", Number(data.data[13].immediatePercentage.replace('%','')), config13);
+            // var config14 = liquidFillGaugeDefaultSettings();
+            // loadLiquidFillGauge("fillgauge14", Number(data.data[14].immediatePercentage.replace('%','')), config14);
+            // var config15 = liquidFillGaugeDefaultSettings();
+            // loadLiquidFillGauge("fillgauge15", Number(data.data[15].immediatePercentage.replace('%','')), config15);
+            // var config16 = liquidFillGaugeDefaultSettings();
+            // loadLiquidFillGauge("fillgauge16", Number(data.data[16].immediatePercentage.replace('%','')), config16);
+            // var config17 = liquidFillGaugeDefaultSettings();
+            // loadLiquidFillGauge("fillgauge17", Number(data.data[17].immediatePercentage.replace('%','')), config17);
+            // var config18 = liquidFillGaugeDefaultSettings();
+            // loadLiquidFillGauge("fillgauge18", Number(data.data[18].immediatePercentage.replace('%','')), config18);
+            // var config19 = liquidFillGaugeDefaultSettings();
+            // loadLiquidFillGauge("fillgauge19", Number(data.data[19].immediatePercentage.replace('%','')), config19);
+            // var config70 = liquidFillGaugeDefaultSettings();
+            // loadLiquidFillGauge("fillgauge20", Number(data.data[20].immediatePercentage.replace('%','')), config70);
 
 
 
