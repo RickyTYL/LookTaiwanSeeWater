e12b8c8cada26051f28b2b0a19a4614fc849d49d
Ray0427 <Ray0427@users.noreply.github.com>
2015-05-17 13:21:03 +0800

    Create select.js

---
+++ js/select.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Title of the document</title>
+
+</head>
+
+<body>
+	<select id="selectReservoir"></select>
+	<label>目前蓄水量可供</label>
+	<select id="mySelect">	</select>
+	<label id="restDays"></label>
+	<label>天的民生用水</label>
+	<br/>
+	<label>有效蓄水量(萬立方公尺)</label>
+	<label id="immediateStorage"></label>
+	<label>生活用水總量(噸)：</label>
+	<label id="domesticConsumption"></label>
+	<label>用水人口：</label>
+	<label id="PopulationServed"></label>
+	<label>每人每日生活用水量(公升)：</label>
+	<label id="dailyDomesticConsumptionOfWaterPerPerson"></label>
+	
+	<br/>
+	<label>以上計算利用2013年用水量以及生活用水占用水比例18%，對即時水庫蓄水量做估算</label>
+	<script src="js/jquery.js"></script>
+	<script src="js/select.js"></script>
+</body>
+
+</html>tg
